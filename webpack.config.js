8f12c37370801b5753ad50a71e0ff5608ac87024
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2016-08-16 23:12:57 +0300

    Add webpack for comparasion

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'hidden-source-map',
+  entry: './app',
+  output: {
+    path: `${__dirname}/build`,
+    filename: 'webpack.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  ]
+}
